b06d0597871a4f5a664a152116d4cee99775cd4a
xiaodongus <xiaodongus@yeah.net>
2019-04-07 12:44:30 +0800

    Update snell.centos.sh

--- snell.centos.sh
+++ snell.centos.sh
@@ -22,7 +22,7 @@ if [ -f ${CONF} ]; then
   mkdir /etc/snell/
   echo "Generating new config..."
   echo "[snell-server]" >>${CONF}
-  echo "listen = 0.0.0.0:443" >>${CONF}
+  echo "listen = 0.0.0.0:12296" >>${CONF}
   echo "psk = ${PSK}" >>${CONF}
   echo "obfs = tls" >>${CONF}
 fi
